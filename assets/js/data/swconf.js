f2b598e8b6e3ece4c9890320ed1c51ff11d8b787
monoanono <monoanono@users.noreply.github.com>
2026-03-04 00:03:49 +0000

    deploy: fd2e187ac041cb0948cd5aee01de1da2c63f71c7

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1772521764',resources: [
+    cacheName: 'chirpy-1772582628',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
